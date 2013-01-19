f40e0e0474148a010dabf81c236a196da5bbaf57
Matej Urbas <matej.urbas@gmail.com>
2013-01-19 14:52:16 +0000

    More fixing after renaming.

---
+++ devel/drivers/HyperblocksMixR/release/modules/ext/create-dependency-links.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/bash
+
+ln -s ../../../../../../external/libs/Openbox.jar .
---
+++ devel/drivers/TPTP/release/modules/ext/create-dependency-links.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+
+ln -s ../../../../../../external/libs/antlr-2.7.5.jar .
+ln -s ../../../../../../external/libs/TptpParser.jar .
