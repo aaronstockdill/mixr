7ff32704f400a9304fd60e459b38614b0f43673e
Matej Urbas <matej.urbas@gmail.com>
2013-11-05 01:36:42 +0000

    Fixed dependencies in MixR projects.

--- devel/drivers/IsabelleMixRDriver/release/modules/ext/create-dependency-links.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/bash
-
-ln -s ~/bin/Isabelle2012/lib/classes/ext/*.jar .
-ln -s ../../../../IsabelleScalaWrapper/dist/libIsaWrapper.jar .
--- devel/drivers/SpeedithMixRDriver/release/modules/ext/create-dependency-links.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-
-ln -s ../../../../SpeedithIsabelle/dist/libSdIsaWrapper.jar .
-ln -s ../../../../../../../speedith/devel/Speedith/dist/lib/apache-commons-cli.jar commons-cli.jar
-ln -s ../../../../../../../speedith/devel/Speedith/dist/Speedith.jar .
-ln -s ../../../../../../../speedith/devel/Speedith/dist/lib/Speedith.Core.jar .
-ln -s ../../../../../../../speedith/devel/Speedith/dist/lib/antlr3-runtime.jar .
-ln -s ../../../../../../../speedith/devel/Speedith/dist/lib/iCircles.jar .
