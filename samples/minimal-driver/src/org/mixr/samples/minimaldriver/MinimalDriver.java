4c9cbbdc3622babd8ead94bfe7338a97065f37cb
Matej Urbas <matej.urbas@gmail.com>
2013-11-17 19:32:38 +0000

    Finished the minimal driver sample.

--- samples/minimal-driver/src/org/mixr/samples/minimaldriver/MinimalDriver.java
+++ samples/minimal-driver/src/org/mixr/samples/minimaldriver/MinimalDriver.java
@@ -1,8 +1,14 @@
 package org.mixr.samples.minimaldriver;
 
+import mixr.components.MixRDriver;
 import org.openide.util.lookup.ServiceProvider;
 
-//@ServiceProvider(service = MixRDriver.class)
-public class MinimalDriver {
+@ServiceProvider(service = MixRDriver.class)
+public class MinimalDriver implements MixRDriver {
+
+    @Override
+    public String getName() {
+        return "Minimal MixR Driver";
+    }
 
 }
